28db2fbfe86d6c5cfd4e6e30707e4bff0c0505d4
DrMayX <mj6367@gmail.com>
2019-07-03 19:32:09 +0200

    Add reload option; remove read config in game for causing problems

--- src/main/java/me/drmayx/voxelmoneymaker/voxelversemoneymaker/commands/MoneyMakerCommand.java
+++ src/main/java/me/drmayx/voxelmoneymaker/voxelversemoneymaker/commands/MoneyMakerCommand.java
@@ -1,13 +1,11 @@
 package me.drmayx.voxelmoneymaker.voxelversemoneymaker.commands;
 
 import me.drmayx.voxelmoneymaker.voxelversemoneymaker.VoxelVerseMoneyMaker;
-import me.drmayx.voxelmoneymaker.voxelversemoneymaker.config.Config;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
-import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
 
 public class MoneyMakerCommand implements CommandExecutor {
 
@@ -20,27 +18,20 @@ public class MoneyMakerCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
 
+        if(!sender.hasPermission("moneymaker.command")){
+            sender.sendMessage("§4You do not have permission to use that command§r");
+            return true;
+        }
         if(args.length < 1){
-            if(sender.hasPermission("moneymaker.command")){
-                return false;
-            }else{
-                sender.sendMessage("§4You do not have permission to use that command§r");
-                return true;
-            }
+            return false;
         }
 
         switch(args[0]){
-            case "readconfig":
-            case "rc":
-                if(sender.hasPermission("moneymaker.command")) {
-                    sender.sendMessage("Current configuration :");
-                    FileConfiguration config = plugin.getConfig();
-                    for (String key : config.getKeys(true)) {
-                        sender.sendMessage(String.format("%s is %s", key, config.get(key).toString()));
-                    }
-                }else{
-                    sender.sendMessage("§4You do not have permission to use that command§r");
-                }
+            case "reload":
+                sender.sendMessage("Reloading the plugin ...");
+                System.out.println("Reloading the plugin. Caused by " + sender.getName());
+                Bukkit.getPluginManager().disablePlugin(plugin);
+                Bukkit.getPluginManager().enablePlugin(plugin);
                 break;
             default:
                 sender.sendMessage("Invalid sub command!");
